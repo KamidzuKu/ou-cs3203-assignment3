74cb8273d4d70fa91939837caafafb5008b6a9aa
Mikhail Kutepov <Mikhail.Kutepov-1@ou.edu>
2023-02-21 21:28:13 -0600

    Third commit

--- src/assignment3/main.java
+++ src/assignment3/main.java
@@ -1,7 +1,18 @@
 package assignment3;
 import java.util.*;
 public class main {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Give me array size and numbers");
+		int[] arr = new int[sc.nextInt()];
+		for(int i = 0; i<arr.length;i++) {
+			arr[i] = sc.nextInt();
+		}
+		int sum = sum(arr);
+		System.out.println(sum);
+		int mult = mult(arr);
+		System.out.println(mult);
+	}
 	public static int sum(int[] arr) {
 		int sum = 0;
 		for(int i = 0; i< arr.length -1;i++) {
