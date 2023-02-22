15073bd1afcb982ea9ec3c813d0a0c5615c25749
Mikhail Kutepov <Mikhail.Kutepov-1@ou.edu>
2023-02-21 21:24:55 -0600

    Second commit

--- src/assignment3/main.java
+++ src/assignment3/main.java
@@ -1,16 +1,18 @@
 package assignment3;
 import java.util.*;
 public class main {
-	public static void main(String[] args) {
-		
-		int[] arr = {3, 4, 5, 6};
-		int sum = sum(arr);
-		
-	}
+
 	public static int sum(int[] arr) {
 		int sum = 0;
 		for(int i = 0; i< arr.length -1;i++) {
 			sum+= arr[i];
+		}
+		return sum;
+	}
+	public static int mult(int[] arr) {
+		int sum = 0;
+		for(int i = 0; i< arr.length -1;i++) {
+			sum = sum * arr[i];
 		}
 		return sum;
 	}
